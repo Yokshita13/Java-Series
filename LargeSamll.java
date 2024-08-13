39d77ee34e218d1757e3352fb77ff394b6c4933b
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2024-08-13 11:17:54 +0530

    LargeSamll.java

---
+++ LargeSamll.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class LargestSmallest {
+    public static void main(String[] args) {
+
+        // Numbers array
+        int numbers[] = new int[]{55, 32, 45, 98, 82, 11, 9, 39, 50};
+
+        // Assign first element of an array to largest and smallest
+        int smallest = numbers[0];
+        int largest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+            } else if (numbers[i] < smallest) {
+                smallest = numbers[i];
+            }
+        }
+
+        System.out.println("Largest Number is: " + largest);
+        System.out.println("Smallest Number is: " + smallest);
+    }
+}
