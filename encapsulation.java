210bb4e8361f19bd36d125e7690dc0580addbc78
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2025-01-02 21:13:17 +0530

    Create encapsulation.java

---
+++ encapsulation.java
@@ -0,0 +1,39 @@
+class Person {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) { 
+            this.age = age;
+        } else {
+            System.out.println("Please enter a valid age.");
+        }
+    }
+}
+
+public class EncapsulationDemo {
+    public static void main(String[] args) {
+
+        Person person = new Person();
+
+        person.setName("John");
+        person.setAge(25);
+
+  
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
