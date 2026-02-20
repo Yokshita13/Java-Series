e7a92a07dfa03d2f3b198faad29fcf44e9decc97
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-02-20 13:31:59 +0530

    Create floyd_triangle.java

---
+++ floyd_triangle.java
@@ -0,0 +1,13 @@
+class floyd_triangle {
+    public static void main(String[] args){
+        int n=4;
+        int num = 1;
+        for(int i=0; i<n; i++){ 
+            for(int j=0; j<i+1; j++){ 
+                System.out.print(num);
+                num++;
+        }
+        System.out.println();
+       }
+    }
+}
