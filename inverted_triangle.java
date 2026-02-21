3bcad3e456f7e9ed0258b4b2e3a4580fd8f96c98
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-02-21 23:32:32 +0530

    Create inverted_triangle.java

---
+++ inverted_triangle.java
@@ -0,0 +1,17 @@
+class inverted_triangle {
+    public static void main(String[] args){
+        int n=4;
+        for(int i=0; i<n; i++){
+            //spaces
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            //numbers
+            for(int j=0; j<n-i; j++){
+                System.out.print(i+1);
+            }
+        System.out.println();
+        }
+
+    }
+}
