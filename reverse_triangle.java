7cef87bf8e0234d2f3b289d18e29f6e69a51886e
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2026-02-21 23:34:24 +0530

    Create reverse_triangle.java

---
+++ reverse_triangle.java
@@ -0,0 +1,11 @@
+public class reverse_triangle {
+    public static void main(String[] args){
+        int n=4;
+        for(int i=0; i<n; i++){ 
+            for(int j=i+1; j>0; j--){ 
+                System.out.print(j);
+        }
+        System.out.println();
+       }
+    }
+}
